b8b0a389549704afe74bb469fe2411aa15d3b454
jonah godspower <104392495+Geefirce@users.noreply.github.com>
2022-07-14 22:28:04 +0000

    challenge 1  and challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0 ;
+   for( let i = 0, x = arrayOfNumbers.length; i < x; i++){
+     sum+= arrayOfNumbers[i];
+    
+   }  return sum;
+  return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
+  let count = 0;
+  for (let i = 0 ; i < arrayOfNumbers.length; i++){
+    if ( arrayOfNumbers[i] % 2 === 0){
+      count++;
+    }
+    
+  }
+    return count;
     return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
